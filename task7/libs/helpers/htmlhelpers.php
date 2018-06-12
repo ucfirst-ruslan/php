20f4163cf789520f9fb85265a56f9bb325a51743
Ruslan <ruslan@mail.com>
2018-06-13 01:32:08 +0300

    add full htmlHelper in task7 and task9

--- task7/libs/helpers/htmlhelpers.php
+++ task7/libs/helpers/htmlhelpers.php
@@ -4,121 +4,171 @@ class HtmlHelpers
 {
 	public static function getDropdown($name, $options, $selected)
 	{
-		$select = '<select name="'.$name.'">';
+		$select = '<select name="' . $name . '">';
 		$select .= '<option selected disabled>Select subj</option>';
-			foreach ($options as $key=>$val)
-			{
-				if ($selected != $key)
-					$select .= '<option value="'.$key.'">'.$val.'</option>';
-				else
-					$select .= '<option selected value="'.$key.'">'.$val.'</option>';
-			}
-		
+		foreach ($options as $key => $val) {
+			if ($selected != $key)
+				$select .= '<option value="' . $key . '">' . $val . '</option>';
+			else
+				$select .= '<option selected value="' . $key . '">' . $val . '</option>';
+		}
+
 		$select .= '</select>';
-		
+
 		return $select;
 	}
-	
-	public static function getSelectMulti($name, $options, $selected)
+
+	/**
+	 * Может работать аналогично предыдущему,
+	 * но развернут до multiple
+	 *
+	 * @param $name string
+	 * @param $selected string
+	 * @param $data array
+	 * @param $multi int
+	 * @return string
+	 */
+	public static function getSelect($name, $data, $selected, $multi)
 	{
-		if (4 < count($options))
-			$size = 4;
-		else 
-			$size = 1;
-		
-		$select = '<select name="'.$name.'" multiple size="'.$size.'">';
-			foreach ($options as $key=>$val)
-			{
-				if ($selected != $key)
-					$select .= '<option value="'.$key.'">'.$val.'</option>';
-				else
-					$select .= '<option selected value="'.$key.'">'.$val.'</option>';
-			}
-		
+		if (is_numeric($multi)) {
+			$multiple = '" multiple size="' . $multi;
+		}
+
+		$select = '<select name="' . $name . $multiple . '">';
+		foreach ($data as $key => $val) {
+			if ($selected != $key)
+				$select .= '<option value="' . $key . '">' . $val . '</option>';
+			else
+				$select .= '<option selected value="' . $key . '">' . $val . '</option>';
+		}
+
 		$select .= '</select>';
-		
+
 		return $select;
 	}
-	
-	public static function getSelectMulti($headName, array$headData, array$datas)
+
+	/**
+	 * @param array $headData [1, 2, 3]
+	 * @param array $datas ['0' => ['q', 'w', 'd']]
+	 * @return string
+	 */
+	public static function getTable(array $headData, array $datas)
 	{
 		$table = '<table>';
-				
-		//['', 'q', 'w', 'd']
-		if (!empty($headData))
-		{
+
+		//Заголовочные ячейки писать так['', 'q', 'w', 'd']
+		$i = 1;
+		if (!empty($headData)) {
 			$table .= '<tr>';
-			foreach ($headData as $val)
-			{
-				$table .= '<th>'.$val.'</th>';
+			foreach ($headData as $val) {
+				$table .= '<th>' . $val . '</th>';
+				$i++;
 			}
 			$table .= '</tr>';
 		}
-		
-		//['0' => ['q', 'w', 'd']]
-		foreach ($datas as $data)
-		{
+
+
+		$a = 1;
+		foreach ($datas as $data) {
 			$table .= '<tr>';
-				foreach ($data as $val)
-				{
-					$table .= '<td>'.$val.'</td>';
-				}
+
+			foreach ($data as $val) {
+
+				if ($i >= $a)
+					$table .= '<td>' . $val . '</td>';
+				else
+					continue;
+
+				$a++;
+			}
 			$table .= '</tr>';
 		}
 		$table .= '</table>';
-		
+
 		return $table;
 	}
-	
+
+	/**
+	 * @param $type
+	 * @param $data
+	 * @return string
+	 */
 	public static function getList($type, $data)
 	{
-		//[ol, A, R, 2]
-		//[ul, disc]
-		if ($type[0] == 'ol')
+		//ol|ul | [ol, 'rever', 4]
+		if (is_array($type) && $type[0] === 'ol')
+		{
+			$list = '<ol ';
+			$listEnd = '</ol>';
+
+			if ($type[1])
+				$list .= 'reversed';
+
+			if ($type[2])
+				$list .= ' start='. $type[2].'"';
+		}
+		else if ($type === 'ol')
 		{
 			$list = '<ol';
 			$listEnd = '</ol>';
-		}	
-		else	
+		}
+		else
 		{
 			$list = '<ul';
 			$listEnd = '</ul>';
 		}
-		
-		
-			
-			
-		$select = '<select name="'.$name.'">';
-		$select .= '<option selected disabled>Select subj</option>';
-			foreach ($options as $key=>$val)
-			{
-				if ($selected != $key)
-					$select .= '<option value="'.$key.'">'.$val.'</option>';
-				else
-					$select .= '<option selected value="'.$key.'">'.$val.'</option>';
-			}
-		
-		$select .= '</select>';
-		
-		return $select;
-	}
-	
-}
-<ol type="A | a | I | i | 1">...</ol>
-<ol reversed>
-<ol type="I" start="4">
-<ol>
-    <li>Чебурашка</li>
-    <li>Крокодил Гена</li>
-    <li>Шапокляк</li>
-  </ol>
+		$list .= '>';
 
-<ul type="disc | circle | square">...</ul>
-<ul>
-    <li>Чебурашка</li>
-    <li>Крокодил Гена</li>
-    <li>Шапокляк</li>
-</ul>
+		foreach ($data as $val) {
+			$list .= '<li>'.$val.'</li>';
+		}
+
+		$list .= $listEnd;
+
+		return $list;
+	}
+
+	public static function getDefinitions(array $data)
+	{
+		$def = '<dl>';
+		foreach ($data as $key => $val) {
+			$def .= '<dt>'. $key .'</dt><dd>'. $val .'</dd>';
+		}
+
+		$def .= '</dl>';
+
+		return $def;
+	}
+
+	//$type string radio|checkbox
+	//$name string (имя группы радиокнопок)
+	//$data array [key=>value]($key передается на сервер)
+	//$check array [key=>check]('key' должен быть равен аналогичному значению из массива $data)
+	public static function getRadio($type, $name,array $data, $check)
+	{
+		$radio = '';
+		$i=1;
+
+		if (!($type === 'radio'))
+			$type = 'checkbox';
+
+		foreach ($data as $key=>$val)
+		{
+			if ($check[$key]) {
+				$radio .= '<input type="'.$type.'" checked id="choice'.$i.'" name="'.$name.'" value="'.$key. '">';
+			}
+			else
+			{
+				$radio .= '<input type="'.$type.'" id="choice'.$i.'" name="'.$name.'" value="'.$key.'">';
+			}
+			$radio .= '<label for="choice'.$i.'">'.$val.'</label><br>';
+			$i++;
+		}
+
+		return $radio;
+	}
+
+}
 
 
 
